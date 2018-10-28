09e9c2e89afb677ccaaad056e33b017581eead3c
John <jonathan.plantey@gmail.com>
2018-10-28 02:48:21 +0100

    wip

---
+++ src/Twig/BootstrapAlerts.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Twig;
+
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
+use Twig_Extension;
+use Twig_SimpleFunction;
+
+class BootstrapAlerts extends Twig_Extension
+{
+    /**
+     * Equivalent en classe bootstrap
+     */
+    const YELLOW    = 'warning';
+    const BLUE      = 'info';
+    const RED       = 'danger';
+    const GREEN     = 'success';
+    /**
+     * @var \Twig_Environment
+     */
+    private $twig;
+
+    public function __construct(\Twig_Environment $twig_Environment)
+    {
+        $this->twig = $twig_Environment;
+    }
+
+
+    /***********************************************************************************************************
+     *                              TWIG EXTENSION
+     ***********************************************************************************************************/
+
+    public function getFunctions()
+    {
+        return [
+            new Twig_SimpleFunction('alert',         [$this, 'alert'],         ['is_safe' => ['html']]),
+            new Twig_SimpleFunction('yellowAlert',   [$this, 'yellowAlert'],   ['is_safe' => ['html']]),
+            new Twig_SimpleFunction('blueAlert',     [$this, 'blueAlert'],     ['is_safe' => ['html']]),
+            new Twig_SimpleFunction('redAlert',      [$this, 'redAlert'],      ['is_safe' => ['html']]),
+            new Twig_SimpleFunction('greenAlert',    [$this, 'greenAlert'],    ['is_safe' => ['html']])
+
+        ];
+    }
+
+    /**
+     * name of the extension
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return "BootstrapAlerts";
+    }
+
+
+    /***********************************************************************************************************
+     *                              TWIG FUNCTIONS
+     ***********************************************************************************************************/
+    /**
+     * @param FlashBagInterface $flashbag
+     * @return string
+     */
+    public function alert(FlashBagInterface $flashbag)
+    {
+        $outputHTML = '';
+
+        foreach ($flashbag->keys() as $type) {
+            foreach ($flashbag->get($type) as $message) {
+                $outputHTML .= $this->renderMessage($message, $type);
+            }
+        }
+
+        return $outputHTML;
+    }
+
+    public function yellowAlert($message)
+    {
+        return $this->renderMessage($message, self::YELLOW);
+    }
+
+    public function blueAlert($message)
+    {
+        return $this->renderMessage($message, self::BLUE);
+    }
+
+    public function redAlert($message)
+    {
+        return $this->renderMessage($message, self::RED);
+    }
+
+    public function greenAlert($message)
+    {
+        return $this->renderMessage($message, self::GREEN);
+    }
+
+    private function renderMessage($message, $class)
+    {
+        $datas              = [];
+        $datas['message']   = $message;
+        $datas['class']     = $class;
+
+        return $this->twig->render('bootstrap_alerts.html.twig', $datas);
+    }
+}
---
+++ src/Twig/PicturesExtension.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Twig;
+
+use App\Entity\Picture;
+use App\Entity\User;
+use Symfony\Component\Routing\Generator\UrlGenerator;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+class PicturesExtension extends \Twig_Extension
+{
+    /**
+     * @var UrlGenerator
+     */
+    private $router;
+
+    public function __construct(UrlGeneratorInterface $urlGenerator)
+    {
+        $this->router = $urlGenerator;
+    }
+
+    /***********************************************************************************************************
+     *                              TWIG EXTENSION
+     ***********************************************************************************************************/
+
+    public function getFilters()
+    {
+        return [
+            new \Twig_SimpleFilter('crop', [$this, 'crop']),
+            new \Twig_SimpleFilter('resize', [$this, 'resize']),
+        ];
+    }
+
+    public function getFunctions()
+    {
+        return [
+            new \Twig_SimpleFunction('picture', [$this, 'picture'], ['is_safe' => ['html']]),
+            new \Twig_SimpleFunction('userAvatar', [$this, 'userAvatar'], ['is_safe' => ['html']]),
+            new \Twig_SimpleFunction('picturePath', [$this, 'path'], ['is_safe' => ['html']]),
+        ];
+    }
+
+    /***********************************************************************************************************
+     *                                  FILTERS
+     ***********************************************************************************************************/
+    public function crop($path, $x, $y = null)
+    {
+        return $path.'?c='.$x.($y ? 'x'.$y:'');
+    }
+
+    public function resize($path, $x, $y = null)
+    {
+        return $path.'?r='.$x.($y ? 'x'.$y:'');
+    }
+
+    /***********************************************************************************************************
+     *                                  FUNCTIONS
+     ***********************************************************************************************************/
+    public function userAvatar(User $user)
+    {
+        if($user->getAvatar()) {
+            return $this->getRoute($user->getAvatar()->getPath());
+        }
+
+        if ($user->getSexe() == 'male') {
+            return $this->getRoute('pictures/default_male.png');
+        }
+        else {
+            return $this->getRoute('pictures/default_female.png');
+        }
+    }
+
+    public function picture(Picture $picture = null)
+    {
+        if ($picture) {
+            return $this->getRoute($picture->getPath());
+        }
+
+        return $this->getRoute('pictures/default_picture.jpg');
+    }
+
+    public function path($path)
+    {
+        return $this->getRoute($path);
+    }
+
+    /***********************************************************************************************************
+     *                              TOOLS
+     ***********************************************************************************************************/
+    /**
+     * @param string $path the path of the picture (ex: $picture->getPath() or "pictures/pic.jpg")
+     */
+    private function getRoute($path)
+    {
+        return $this->router->generate('media.pictures', ['path' => $path]);
+    }
+}
